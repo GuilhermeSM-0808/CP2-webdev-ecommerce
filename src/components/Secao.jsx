cf9fde499766d383c6175e66155d59c268779958
GuilhermeSM <guilhermesm.0808@gmail.com>
2025-10-16 22:50:37 -0300

    fix: HTML dentro do <P> FIXED

--- src/components/Secao.jsx
+++ src/components/Secao.jsx
@@ -3,7 +3,7 @@ export default function Secao({ title, children }) {
     <div className="bg-blue-200 p-4 m-6 rounded-lg shadow transition">
       <h3 className="text-lg font-semibold mb-2">{title}</h3>
       <h5>Produtos em destaque</h5>
-      <p className="text-gray-700">{children}</p>
+      {children}
     </div>
   );
 }
